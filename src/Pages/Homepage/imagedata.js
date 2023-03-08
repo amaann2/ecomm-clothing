e48742e71e237f80290729ed6033666ebe2951f1
Amaan Ansari <ansari028amaan@gmail.com>
2023-03-08 20:34:37 +0530

    aded image

--- src/Pages/Homepage/imagedata.js
+++ src/Pages/Homepage/imagedata.js
@@ -1,8 +1,10 @@
 import img1 from "../../assests/banner1.png";
 import img2 from "../../assests/banner2.png";
 import img3 from "../../assests/banner3.png";
-import img4 from "../../assests/banner4.png";
-import img5 from "../../assests/banner5.png";
-import img6 from "../../assests/banner6.png";
+// import img4 from "../../assests/banner4.png";
+// import img5 from "../../assests/banner5.png";
+import img5 from "../../assests/banner8.png";
+// import img6 from "../../assests/banner6.png";
+import img6 from "../../assests/banner7.png";
 
-export const imgData = [img1, img2, img3, img4, img5, img6];
+export const imgData = [img1, img2, img3,img5, img6];
